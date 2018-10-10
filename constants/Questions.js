f11e46224f0ed5196cbcfa74039f80217b587cc1
Robin van Leeuwen <rvanleeuwen@jouwomgeving.nl>
2018-10-10 15:26:15 +0200

    Add some questions

---
+++ constants/Questions.js
@@ -0,0 +1,100 @@
+export default {
+  "Geslacht ": ["Vrouw", "Man"],
+  "Herkomst ouders": [
+    "Beide ouders Nederlands",
+    "Beide ouders niet Nederlands",
+    "Een van de ouders Nederlands en de andere niet Nederlands"
+  ],
+  "Type huishouden": [
+    "Gehuwd paar met kinderen",
+    "Eenouderhuishouden",
+    "Eenpersoonshuishouden",
+    "Gehuwd paar zonder kinderen",
+    "Institutioneel huishouden",
+    "Niet-gehuwd paar met kinderen",
+    "Niet-gehuwd paar zonder kinderen",
+    "Overig huishouden",
+    "Onbekend huishouden"
+  ],
+  "Gescheiden ouders": ["Nee", "Ja"],
+  "Voortijdig schoolverlaten": [
+    "Door in onderwijs",
+    "Onbekend",
+    "Niet van toepassing",
+    "Uit onderwijs met startkwalificatie",
+    "Voortijdig schoolverlaten"
+  ],
+  "Soort onderwijs": [
+    "Regulier onderwijs",
+    "Onbekend",
+    "Niet-regulier onderwijs"
+  ],
+  "Soort woning": [
+    "Eigen woning",
+    "Huurwoning met huurtoeslag",
+    "Huurwoning zonder huurtoeslag",
+    "Onbekend"
+  ],
+  "Werk vader": [
+    "Werkend of overig actief",
+    "Geen werk en niet actief",
+    "Onbekend"
+  ],
+  "Werk moeder": [
+    "Werkend of overig actief",
+    "Geen werk en niet actief",
+    "Onbekend"
+  ],
+  Slachtoffer: ["Nee", "Ja"],
+  "Traject vooraf": [
+    "Geen jeugdhulp zonder verblijf gehad",
+    "Jeugdhulp zonder verblijf gehad"
+  ],
+  "Leeftijd moeder bij geboorte": [
+    "Jonger dan 20 jaar",
+    "20 tot 25 jaar",
+    "25 tot 30 jaar",
+    "30 tot 35 jaar",
+    "35 tot 40 jaar",
+    "40 jaar en ouder"
+  ],
+  "Leeftijd vader bij geboorte": [
+    "Jonger dan 25 jaar",
+    "25 tot 30 jaar",
+    "30 tot 35 jaar",
+    "35 tot 40 jaar",
+    "40 jaar en ouder"
+  ],
+  "Vader of moeder verdacht": ["Nee", "Ja"],
+  "Kind verdacht": ["Nee", "Ja"],
+  "Halt delict": ["Nee", "Ja"],
+  "Verandering onderwijs niveau": [
+    "Opschalen of gelijk blijvend",
+    "Afschalen",
+    "Onbekend"
+  ],
+  "Leeftijdsverschil ouders": ["Minder dan 5 jaar", "Meer dan 5 jaar"],
+  "Actueel onderwijs niveau": [
+    "Basisonderwijs",
+    "Onderwijs onbekend",
+    "Vmbo b/k, mbo 1 of mbo 2",
+    "Vmbo g/t, mbo 3 of mbo 4",
+    "Havo onderbouw, Havo bovenbouw of Hbo bachelor",
+    "Vwo of Wo bachelor"
+  ],
+  "Hoogst behaalde diploma vader": [
+    "Onderwijs onbekend",
+    "Vmbo b/k, mbo 1 of mbo 2",
+    "Vmbo g/t, mbo 3 of mbo 4",
+    "Havo onderbouw, Havo bovenbouw, Hbo bachelor of Hbo master",
+    "Vwo, Wo bachelor of Wo master"
+  ],
+  "Hoogst behaalde diploma moeder": [
+    "Onderwijs onbekend",
+    "Vmbo b/k, mbo 1 of mbo 2",
+    "Vmbo g/t, mbo 3 of mbo 4",
+    "Havo onderbouw, Havo bovenbouw, Hbo bachelor of Hbo master",
+    "Vwo, Wo bachelor of Wo master"
+  ],
+  "Zwaar traject": ["Nee", "Ja"]
+};
